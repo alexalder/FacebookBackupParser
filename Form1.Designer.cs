58abb3547826a7bc9d155bdafd31521891c59630
Alessandro Pettazzoni <alessandropettazzoni@gmail.com>
2020-08-01 15:53:51 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FacebookBackupParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFolderButton = new System.Windows.Forms.Button();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.startButton = new System.Windows.Forms.Button();
+            this.backup = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chooseFolderButton
+            // 
+            this.chooseFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.chooseFolderButton.Name = "chooseFolderButton";
+            this.chooseFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.chooseFolderButton.TabIndex = 0;
+            this.chooseFolderButton.Text = "Chat Folder";
+            this.chooseFolderButton.UseVisualStyleBackColor = true;
+            this.chooseFolderButton.Click += new System.EventHandler(this.chooseFolderButton_Click);
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Location = new System.Drawing.Point(125, 14);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(259, 20);
+            this.folderTextBox.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(264, 54);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // backup
+            // 
+            this.backup.AutoSize = true;
+            this.backup.Checked = true;
+            this.backup.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.backup.Location = new System.Drawing.Point(32, 54);
+            this.backup.Name = "backup";
+            this.backup.Size = new System.Drawing.Size(93, 17);
+            this.backup.TabIndex = 5;
+            this.backup.Text = "Make Backup";
+            this.backup.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 97);
+            this.Controls.Add(this.backup);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.chooseFolderButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseFolderButton;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.CheckBox backup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookBackupParser
+{
+    public partial class Form1 : Form
+    {
+        string inputPath;
+        readonly string photosFolderName = "photos";
+        readonly string htmlFileName = "message.html";
+        readonly string newFolderName = "parsedPhotos";
+        readonly string imageFormat = ".jpg";
+        readonly string dateFormat = "yyyy-MM-dd_HH'h'-mm'm'-ss's'";
+        HtmlAgilityPack.HtmlDocument document;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderTextBox.Text = folderBrowserDialog1.SelectedPath;
+                inputPath = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(inputPath))
+            {
+                string htmlPath = Path.Combine(inputPath, htmlFileName);
+                string loaded = File.ReadAllText(htmlPath);
+                document = new HtmlAgilityPack.HtmlDocument();
+                document.LoadHtml(loaded);
+
+                string directoryPath = Path.Combine(inputPath, photosFolderName);
+                string outputFolder = directoryPath;
+
+                if (backup.Checked)
+                {
+                    outputFolder = Path.Combine(inputPath, newFolderName);
+                    Directory.CreateDirectory(outputFolder);
+                }
+                    
+
+                foreach (String fileName in Directory.GetFiles(directoryPath))
+                {
+                    string originalFileName = fileName;
+                    string dateString = searchHtml(Path.GetFileName(fileName));
+                    DateTime date = DateTime.Parse(dateString);
+                    string dateName = date.ToString(dateFormat) + imageFormat;
+                    string newFileName = Path.Combine(outputFolder, dateName);
+
+                    int tentative = 0;
+                    while (File.Exists(newFileName))
+                    {
+                        newFileName = Path.Combine(outputFolder, dateName + tentative + imageFormat);
+                        tentative++;
+                    }
+
+                    if (backup.Checked)
+                        File.Copy(originalFileName, newFileName);
+                    else
+                        File.Move(originalFileName, newFileName);
+                }
+            }
+        }
+
+        private string searchHtml(string image)
+        {
+            HtmlAgilityPack.HtmlNode node =
+    document.DocumentNode.Descendants("img")
+                .Where(e => e.GetAttributeValue("src", null).Contains(image))
+                .FirstOrDefault();
+
+            return node.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.SelectSingleNode(".//div[@class='_3-94 _2lem']").InnerText;
+        }
+    }
+}
