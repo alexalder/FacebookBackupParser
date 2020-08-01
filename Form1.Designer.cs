fd0d674a09a2443266eb36d19b62dbc96c142516
Alessandro Pettazzoni <alessandropettazzoni@gmail.com>
2020-08-01 16:56:47 +0200

    Add video parsing capability

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.startButton = new System.Windows.Forms.Button();
             this.backup = new System.Windows.Forms.CheckBox();
+            this.progressBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // chooseFolderButton
@@ -48,10 +49,13 @@
             // 
             // folderTextBox
             // 
+            this.folderTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.folderTextBox.Location = new System.Drawing.Point(125, 14);
             this.folderTextBox.Name = "folderTextBox";
             this.folderTextBox.Size = new System.Drawing.Size(259, 20);
             this.folderTextBox.TabIndex = 1;
+            this.folderTextBox.Text = "Select the facebookExport\\messages\\user folder";
+            this.folderTextBox.Click += new System.EventHandler(this.folderTextBox_Click);
             // 
             // openFileDialog1
             // 
@@ -59,7 +63,7 @@
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(264, 54);
+            this.startButton.Location = new System.Drawing.Point(309, 108);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(75, 23);
             this.startButton.TabIndex = 4;
@@ -79,11 +83,22 @@
             this.backup.Text = "Make Backup";
             this.backup.UseVisualStyleBackColor = true;
             // 
+            // progressBox
+            // 
+            this.progressBox.Enabled = false;
+            this.progressBox.Location = new System.Drawing.Point(13, 110);
+            this.progressBox.Name = "progressBox";
+            this.progressBox.ReadOnly = true;
+            this.progressBox.Size = new System.Drawing.Size(257, 20);
+            this.progressBox.TabIndex = 6;
+            this.progressBox.Text = "Press Start";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 97);
+            this.ClientSize = new System.Drawing.Size(397, 143);
+            this.Controls.Add(this.progressBox);
             this.Controls.Add(this.backup);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.folderTextBox);
@@ -103,6 +118,7 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.CheckBox backup;
+        private System.Windows.Forms.TextBox progressBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,12 @@ namespace FacebookBackupParser
     {
         string inputPath;
         readonly string photosFolderName = "photos";
+        readonly string videosFolderName = "videos";
         readonly string htmlFileName = "message.html";
-        readonly string newFolderName = "parsedPhotos";
+        readonly string newPhotoFolderName = "parsedPhotos";
+        readonly string newVideoFolderName = "parsedVideos";
         readonly string imageFormat = ".jpg";
+        readonly string videoFormat = ".mp4";
         readonly string dateFormat = "yyyy-MM-dd_HH'h'-mm'm'-ss's'";
         HtmlAgilityPack.HtmlDocument document;
 
@@ -37,30 +40,64 @@ namespace FacebookBackupParser
 
         private void startButton_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(inputPath))
+            {
+                string inputText = folderTextBox.Text;
+                if (Directory.Exists(inputText))
+                {
+                    inputPath = inputText;
+                }
+            }
+
             if (!String.IsNullOrEmpty(inputPath))
             {
-                string htmlPath = Path.Combine(inputPath, htmlFileName);
-                string loaded = File.ReadAllText(htmlPath);
-                document = new HtmlAgilityPack.HtmlDocument();
-                document.LoadHtml(loaded);
+                progressBox.Text = "Parsing HTML file...";
+                progressBox.Refresh();
 
-                string directoryPath = Path.Combine(inputPath, photosFolderName);
+                ParseHTML();
+
+                progressBox.Text = "Writing photo information...";
+                progressBox.Refresh();
+
+                WritePhotos();
+
+                progressBox.Text = "Writing video information...";
+                progressBox.Refresh();
+
+                WriteVideos();
+                
+                progressBox.Text = "Completed!";
+            }
+        }
+
+        private void ParseHTML()
+        {
+            string htmlPath = Path.Combine(inputPath, htmlFileName);
+            string loaded = File.ReadAllText(htmlPath);
+            document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(loaded);
+        }
+
+        private void WritePhotos()
+        {
+            string directoryPath = Path.Combine(inputPath, photosFolderName);
+            if (Directory.Exists(directoryPath))
+            {
                 string outputFolder = directoryPath;
-
                 if (backup.Checked)
                 {
-                    outputFolder = Path.Combine(inputPath, newFolderName);
+                    outputFolder = Path.Combine(inputPath, newPhotoFolderName);
                     Directory.CreateDirectory(outputFolder);
                 }
-                    
+
 
                 foreach (String fileName in Directory.GetFiles(directoryPath))
                 {
                     string originalFileName = fileName;
-                    string dateString = searchHtml(Path.GetFileName(fileName));
+                    string dateString = searchHtml(image: Path.GetFileName(fileName));
                     DateTime date = DateTime.Parse(dateString);
-                    string dateName = date.ToString(dateFormat) + imageFormat;
-                    string newFileName = Path.Combine(outputFolder, dateName);
+                    string dateName = date.ToString(dateFormat);
+                    string newFileName = Path.Combine(outputFolder, dateName + imageFormat);
 
                     int tentative = 0;
                     while (File.Exists(newFileName))
@@ -77,14 +114,69 @@ namespace FacebookBackupParser
             }
         }
 
-        private string searchHtml(string image)
+        private void WriteVideos()
         {
-            HtmlAgilityPack.HtmlNode node =
-    document.DocumentNode.Descendants("img")
-                .Where(e => e.GetAttributeValue("src", null).Contains(image))
-                .FirstOrDefault();
+            string directoryPath = Path.Combine(inputPath, videosFolderName);
+            if (Directory.Exists(directoryPath))
+            {
+                string outputFolder = directoryPath;
+                if (backup.Checked)
+                {
+                    outputFolder = Path.Combine(inputPath, newVideoFolderName);
+                    Directory.CreateDirectory(outputFolder);
+                }
 
-            return node.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.SelectSingleNode(".//div[@class='_3-94 _2lem']").InnerText;
+
+                foreach (String fileName in Directory.GetFiles(directoryPath))
+                {
+                    string originalFileName = fileName;
+                    string dateString = searchHtml(video: Path.GetFileName(fileName));
+                    DateTime date = DateTime.Parse(dateString);
+                    string dateName = date.ToString(dateFormat);
+                    string newFileName = Path.Combine(outputFolder, dateName + videoFormat);
+
+                    int tentative = 0;
+                    while (File.Exists(newFileName))
+                    {
+                        newFileName = Path.Combine(outputFolder, dateName + tentative + videoFormat);
+                        tentative++;
+                    }
+
+                    if (backup.Checked)
+                        File.Copy(originalFileName, newFileName);
+                    else
+                        File.Move(originalFileName, newFileName);
+                }
+            }
+        }
+
+        private string searchHtml(string image = null, string video = null)
+        {
+            if (image != null)
+            {
+                HtmlAgilityPack.HtmlNode node =
+                document.DocumentNode.Descendants("img")
+                    .Where(e => e.GetAttributeValue("src", null).Contains(image))
+                    .FirstOrDefault();
+
+                return node.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.SelectSingleNode(".//div[@class='_3-94 _2lem']").InnerText;
+            }
+            else if (video != null)
+            {
+                HtmlAgilityPack.HtmlNode node =
+                document.DocumentNode.Descendants("video")
+                    .Where(e => e.GetAttributeValue("src", null).Contains(video))
+                    .FirstOrDefault();
+
+                return node.ParentNode.ParentNode.ParentNode.ParentNode.SelectSingleNode(".//div[@class='_3-94 _2lem']").InnerText;
+            }
+            else
+                throw new ArgumentException();
+        }
+
+        private void folderTextBox_Click(object sender, EventArgs e)
+        {
+            folderTextBox.Clear();
         }
     }
 }
